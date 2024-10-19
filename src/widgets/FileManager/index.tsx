2410d211bb3e16e8b53194866749a5cab1d1b9db
Andrii Telenko <mangolik931@gmail.com>
2024-10-19 19:06:49 +0200

    added locales for latest folder feature

--- src/widgets/FileManager/index.tsx
+++ src/widgets/FileManager/index.tsx
@@ -1,5 +1,5 @@
 import { useFileManager } from './FileManagerContext';
-
+// @TODO Andrii locales for latest folder feature
 export { default } from './FileManager';
 export { useFileManager };
 export { getRouteMetadatas } from './FileGuiHelper';
