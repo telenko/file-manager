fd1c337d4818ec829ad5cf1018babe8de9f6861c
Andrii Telenko <mangolik931@gmail.com>
2025-07-08 15:56:28 +0200

    video player stabilized

--- src/common/components/VideoViewer/index.tsx
+++ src/common/components/VideoViewer/index.tsx
@@ -1,46 +1,18 @@
 import React, { useEffect, useState } from 'react';
-// @ts-ignore
-// import Video from 'react-native-video-controls';
-import Video, { ViewType } from 'react-native-video'
+import Video from 'react-native-video'
 import { DirItem, FileApi } from '../../../services/FileApi';
-import { ImageBackground, View } from 'react-native';
-import { IconButton, Portal } from 'react-native-paper';
+import { useWindowDimensions, View } from 'react-native';
 import { Cache } from '../../../services/Cache';
-// class CustomizedVideo extends Video {
-//   renderTopControls() {
-//     return null;
-//   }
-//   renderBottomControls() {
-//     // @ts-ignore
-//     if (this.props.paused) {
-//       return null;
-//     }
-//     return (
-//       <Portal>
-//         <View
-//           style={{
-//             position: 'absolute',
-//             bottom: 0,
-//             left: 0,
-//             right: 0,
-//             backgroundColor: 'rgba(120,120,120,0.5)',
-//           }}>
-//           {super.renderBottomControls()}
-//         </View>
-//       </Portal>
-//     );
-//   }
-// }
+import LoadingIndicator from '../LoadingIndicator';
 
 const VideoViewer: React.FC<{
   file: Partial<DirItem>;
   activeFile?: DirItem;
   onActive?: (v: boolean) => void;
 }> = ({ file, activeFile, onActive }) => {
-  const isCurrentViewable = activeFile?.path === file.path;
-  const [paused, setPaused] = useState(true);
   const [preview, setPreview] = useState<string | null>('');
-  const [wasPlayed, setWasPlayed] = useState(false);
+  const [videoSize, setVideoSize] = useState({ width: 0, height: 0 });
+  const { width, height } = useWindowDimensions();
 
   useEffect(() => {
     if (!file.path) {
@@ -53,138 +25,49 @@ const VideoViewer: React.FC<{
       // @ts-ignore
       FileApi.makeVideoPreview(file)
         .then(setPreview)
-        .catch(() => {});
+        .catch(() => { });
     }
   }, [file?.path]);
 
-  useEffect(() => {
-    if (!paused && !wasPlayed) {
-      setWasPlayed(true);
-    }
-  }, [paused]);
-
-  useEffect(() => {
-    if (!isCurrentViewable) {
-      setPaused(true);
-    }
-  }, [isCurrentViewable]);
-
-  const playIcon = (
-    <IconButton
-      onPress={() => setPaused(!paused)}
-      size={90}
-      iconColor="#fff"
-      style={{ height: 90 }}
-      icon={'play-circle'}
-    />
-  );
-
-  const previewLayout = preview ? (
-    <ImageBackground
-      source={{ uri: preview }}
-      resizeMode="contain"
-      style={{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'hidden',
-      }}>
-      {playIcon}
-    </ImageBackground>
-  ) : (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'hidden',
-        backgroundColor: '#fafafa',
-      }}>
-      {playIcon}
-    </View>
-  );
+  const ratio = videoSize.width / videoSize.height;
+  const hasRatio = ratio > 0;
+  const style = {} as any;
+  if (hasRatio) {
+    style.aspectRatio = ratio;
+  }
+  if (width > height) {
+    style.height = hasRatio ? '100%' : 0;
+  } else {
+    style.width = hasRatio ? '100%' : 0;
+  }
 
   return <View style={{
-            flex: 1,
-            width: '100%',
-            height: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-            <Video controls
-              source={{ uri: `file://${file.path}` }}
-              style={{ width: '100%', aspectRatio: 16 / 9 }}
-              resizeMode='contain'
-              poster={{
-                source: { uri: preview ?? undefined },
-                resizeMode: "contain",
-              }}
-            />
-          </View>
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }}>
+    {!hasRatio ? <LoadingIndicator size={100} /> : null}
+    <Video
+      controls
+      source={{ uri: `file://${file.path}` }}
+      style={style}
+      resizeMode='contain'
+      onLoad={(data) => {
+        const { width, height } = data.naturalSize;
+        if (width && height) {
+          setVideoSize({ width, height });
+        }
+      }}
+      poster={{
+        source: { uri: preview ?? undefined },
+        resizeMode: "contain",
+      }}
+      paused
+    />
+  </View>
 
-  return (
-    <>
-      {isCurrentViewable ? (
-        <>
-          {/* @ts-ignore */}
-          <Video
-            // disableFullscreen
-            // disableBack
-            // disableVolume
-            controls
-            // onPause={() => setPaused(true)}
-            // onPlay={() => {
-            //   setPaused(false);
-            // }}
-            // showHours
-            paused={paused}
-            source={{ uri: `file://${file.path}` }}
-            // videoStyle={{ backgroundColor: '#fff' }}
-            // containerStyle={{ backgroundColor: '#fff' }}
-            // style={{ width: '100%', height: '100%' }}
-            resizeMode='contain'
-            poster={{
-              source: { uri: preview ?? undefined },
-              resizeMode: "contain",
-            }}
-          />
-          {wasPlayed ? (
-            paused ? (
-              <View
-                style={{
-                  position: 'absolute',
-                  left: 0,
-                  top: 0,
-                  right: 0,
-                  bottom: 0,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                {playIcon}
-              </View>
-            ) : null
-          ) : (
-            <View
-              style={{
-                position: 'absolute',
-                left: 0,
-                top: 0,
-                right: 0,
-                bottom: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              {preview ? previewLayout : playIcon}
-            </View>
-          )}
-        </>
-      ) : (
-        previewLayout
-      )}
-    </>
-  );
 };
 
 export default VideoViewer;
